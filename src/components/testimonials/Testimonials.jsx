b3f9ad83fd424cd162cbc8bc1a6d4cbda1d7cbcc
pathan-Hameed <150128705+pathan-Hameed@users.noreply.github.com>
2025-07-17 12:29:27 +0530

    Update Testimonials.jsx

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "./testimonials.css";
+import "./Testimonials.css";
 import alex from "../../assets/alexcoach.jpg";
 
 const testimonial = [
